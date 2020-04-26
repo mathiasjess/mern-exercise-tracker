58822bb9221e8b44fe5b044706208a58654623af
jessbernmathias <47259795+jessbernmathias@users.noreply.github.com>
2020-04-26 11:29:08 -0700

    Setting up the front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router , Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //This custom tag, creates routes for various urls sepcified
+    <Router>
+      <Navbar />
+        <br/>
+          <Route path = "/" exact component = {ExerciseList}></Route>
+          <Route path = "/edit/:id" component = {EditExercise}></Route>
+          <Route path = "/create" component = {CreateExercise}></Route>
+          <Route path = "/user" component = {CreateUser}></Route>  
+    </Router>
   );
 }
 
